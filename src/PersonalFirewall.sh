7e877a92a9ba42fd58c52b5ac27315bc6b433dcd
Nishan Vivek <roachx@gmail.com>
2016-01-27 20:09:26 -0800

    Forward Drops

--- src/PersonalFirewall.sh
+++ src/PersonalFirewall.sh
@@ -53,4 +53,39 @@ $IPT -t mangle --policy PREROUTING DROP
 $IPT -t mangle --policy OUTPUT DROP
 
 
+# Test for stealth scans and TCP state flags
+#  Unclean
+$IPT -A INPUT -m unclean -j DROP
+#  All  of  the  bits  are  cleared
+$IPT -A INPUT -p tcp --tcp-flags ALL NONE -j DROP
+#  SYN  and  FIN  are  both  set
+$IPT -A INPUT -p tcp --tcp-flags SYN,FIN SYN,FIN -j DROP
+#  SYN  and  RST  are  both  set
+$IPT -A INPUT -p tcp --tcp-flags SYN,RST SYN,RST -j DROP
+#  FIN  and  RST  are  both  set
+$IPT -A INPUT -p tcp --tcp-flags FIN,RST FIN,RST -j DROP
+#  FIN  is  the  only  bit  set,  without  the  expected  accompanying ACK
+$IPT -A INPUT -p tcp --tcp-flags ACK,FIN FIN -j DROP
+#  PSH  is  the  only  bit  set,  without  the  expected  accompanying ACK
+$IPT -A INPUT -p tcp --tcp-flags ACK,PSH PSH -j DROP
+#  URG  is  the  only  bit  set,  without  the  expected  accompanying ACK
+$IPT -A INPUT -p tcp --tcp-flags ACK,URG URG -j DROP
+
+
+
+
+
+#Permit inbound/outbound ssh packets
+
+#Permit inbound/outbound www packets
+
+#Drop inbound traffic to port 80 (http) from source ports less than 1024
+
+#Drop all incoming packets from reserved port 0 as well as outbound traffic to port 0.
+
+#Create a set of user-defined chains that will implement accounting rules to keep track of www, ssh traffic, versus the rest of the traffic on your system.
+
+#You must ensure the the firewall drops all inbound SYN packets, unless there is a rule that permits inbound traffic.
+
+#Remember to allow DNS and DHCP traffic through so that your machine can function properly
 
