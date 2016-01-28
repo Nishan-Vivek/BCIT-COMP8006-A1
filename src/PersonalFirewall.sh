0c9dae53b76d85c768ca013e37d554b46cb90aed
Nishan Vivek <roachx@gmail.com>
2016-01-28 01:28:03 -0800

    Added OTHin and OTHout chains for accounting

--- src/PersonalFirewall.sh
+++ src/PersonalFirewall.sh
@@ -53,14 +53,17 @@ $IPT -N SSHin
 $IPT -N SSHout
 $IPT -N WWWin
 $IPT -N WWWout
-
+$IPT -N OTHin
+$IPT -N OTHout
+$IPT -A OTHout -j ACCEPT
+$IPT -A OTHin -j ACCEPT
 
 # Allow unlimited traffic on loopback interface
 $IPT -A INPUT -i lo -j ACCEPT
 $IPT -A OUTPUT -o lo -j ACCEPT
 
 # Allow ICMP
-iptables -A INPUT -p icmp -j ACCEPT
+iptables -A INPUT -p icmp -j OTHin
 iptables -A OUTPUT -p icmp -j ACCEPT
 
 #Allow DNS lookups
@@ -69,20 +72,20 @@ iptables -A OUTPUT -p icmp -j ACCEPT
 for ip in $NAMESERVER
 do
 	#echo "Allowing DNS lookups (tcp, udp port 53) to server '$ip'"
-	$IPT -A OUTPUT -p udp -d $ip --dport 53 -m state --state NEW,ESTABLISHED -j ACCEPT
-	$IPT -A INPUT  -p udp -s $ip --sport 53 -m state --state ESTABLISHED     -j ACCEPT
-	$IPT -A OUTPUT -p tcp -d $ip --dport 53 -m state --state NEW,ESTABLISHED -j ACCEPT
-	$IPT -A INPUT  -p tcp -s $ip --sport 53 -m state --state ESTABLISHED     -j ACCEPT
+	$IPT -A OUTPUT -p udp -d $ip --dport 53 -m state --state NEW,ESTABLISHED -j OTHout
+	$IPT -A INPUT  -p udp -s $ip --sport 53 -m state --state ESTABLISHED     -j OTHin
+	$IPT -A OUTPUT -p tcp -d $ip --dport 53 -m state --state NEW,ESTABLISHED -j OTHout
+	$IPT -A INPUT  -p tcp -s $ip --sport 53 -m state --state ESTABLISHED     -j OTHin
 done
 
 #DHCP
-$IPT -A OUTPUT -o $INTERNET -p udp -s $BROADCAST_SRC --sport 68 -d $BROADCAST_DEST --dport 67 -j ACCEPT
-$IPT -A INPUT -i $INTERNET -p udp -s $BROADCAST_SRC --sport 67 -d $BROADCAST_DEST --dport 68 -j ACCEPT
-$IPT -A OUTPUT -o $INTERNET -p udp -s $BROADCAST_SRC --sport 68 -d $DHCP_SERVER --dport 67 -j ACCEPT
-$IPT -A INPUT -i $INTERNET -p udp -s $DHCP_SERVER --sport 67 -d $BROADCAST_DEST --dport 68 -j ACCEPT
-$IPT -A INPUT -i $INTERNET -p udp -s $DHCP_SERVER --sport 67 --dport 68 -j ACCEPT
-$IPT -A OUTPUT -o $INTERNET -p udp -s $IPADDR --sport 68 -d $DHCP_SERVER --dport 67 -j ACCEPT
-$IPT -A INPUT -i $INTERNET -p udp -s $DHCP_SERVER --sport 67 -d $IPADDR --dport 68 -j ACCEPT
+$IPT -A OUTPUT -o $INTERNET -p udp -s $BROADCAST_SRC --sport 68 -d $BROADCAST_DEST --dport 67 -j OTHout
+$IPT -A INPUT -i $INTERNET -p udp -s $BROADCAST_SRC --sport 67 -d $BROADCAST_DEST --dport 68 -j OTHin
+$IPT -A OUTPUT -o $INTERNET -p udp -s $BROADCAST_SRC --sport 68 -d $DHCP_SERVER --dport 67 -j OTHout
+$IPT -A INPUT -i $INTERNET -p udp -s $DHCP_SERVER --sport 67 -d $BROADCAST_DEST --dport 68 -j OTHin
+$IPT -A INPUT -i $INTERNET -p udp -s $DHCP_SERVER --sport 67 --dport 68 -j OTHin
+$IPT -A OUTPUT -o $INTERNET -p udp -s $IPADDR --sport 68 -d $DHCP_SERVER --dport 67 -j OTHout
+$IPT -A INPUT -i $INTERNET -p udp -s $DHCP_SERVER --sport 67 -d $IPADDR --dport 68 -j OTHin
 
 
 #Drop all incoming packets from reserved port 0 as well as outbound traffic to port 0.
